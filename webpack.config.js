7bed9cfa7f3d134808451083b9326449c998a171
pbelko <pbelko@skytracking.ru>
2025-08-04 17:26:08 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 3000,
+        hot: true,
+    },
+    mode: 'development',
+};
